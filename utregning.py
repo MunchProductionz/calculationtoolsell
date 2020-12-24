1a7b579a353d55755becb9afc700def79fe6a5f5
Henrik <henrrb@stud.ntnu.no>
2020-12-24 02:14:48 +0100

    changed utregning.py

--- utregning.py
+++ utregning.py
@@ -1,8 +1,10 @@
 from formating import f2
 
-def salg ():
+def selg (ordrestørrelse, ordrestørrelse_prosent, risiko_ordre, risiko_total):
 
     #Inputs
     print('Vennligst skriv inn ønsket ordre.')
     pt = float(input('Pris per aksje: '))
     antall = int(input('Antall aksjer: '))
+
+    
