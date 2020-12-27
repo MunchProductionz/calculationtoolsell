b56f93ea8d7635ee8e5d2c3b20c57960ccf6c9c0
Henrik <henrrb@stud.ntnu.no>
2020-12-27 01:13:37 +0100

    added print.py and changed utregning.py

--- utregning.py
+++ utregning.py
@@ -1,6 +1,6 @@
 from formating import f2
 
-def selg (ordrestørrelse, ordrestørrelse_prosent, risiko_ordre, risiko_total):
+def utregning_selg (liste_utregning_kjøp):
 
     #Inputs
     print('Vennligst skriv inn ønsket ordre.')
@@ -8,3 +8,23 @@ def selg (ordrestørrelse, ordrestørrelse_prosent, risiko_ordre, risiko_total):
     antall = int(input('Antall aksjer: '))
 
     
+    #Utregning
+
+    #Total profit %
+
+
+
+    #Risiko av total %
+    risiko_total_f = liste_utregning_kjøp[4]
+
+    #EOC
+    
+
+
+
+    #Outputs
+    print(f'Total profit %: {ordrestørrelse}')
+    print(f'Risiko av total %: {}%')
+    print(f'Risk / Reward: {risk_reward} kr')
+    print(f'EOC: {eoc}%')
+
